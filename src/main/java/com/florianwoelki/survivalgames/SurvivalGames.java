5cea443a16d0dcbe665467456aff622f2a64a6eb
sgtflow <sgtflow.yt@gmail.com>
2017-04-11 11:57:55 +0200

    + Added SurvivalGames

---
+++ src/main/java/com/florianwoelki/survivalgames/SurvivalGames.java
@@ -0,0 +1,29 @@
+package com.florianwoelki.survivalgames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.florianwoelki.minigameapi.MinigameAPI;
+import com.florianwoelki.minigameapi.api.Minigame;
+import com.florianwoelki.minigameapi.api.StopReason;
+
+public class SurvivalGames extends JavaPlugin implements Minigame {
+
+	@Override
+	public void onEnable() {
+		MinigameAPI.getInstance().initializeMinigame(this);
+		MinigameAPI.getInstance().setMinigameName("SurvivalGames");
+		MinigameAPI.getInstance().setChatPrefix("SurvivalGames");
+		MinigameAPI.getInstance().setBlockChangesEnabled(true);
+		MinigameAPI.getInstance().setAllowSpectatorDeath(true);
+		MinigameAPI.getInstance().enableMapVoting();
+	}
+	
+	@Override
+	public void startGame() {
+	}
+
+	@Override
+	public void stopGame(StopReason stopReason) {
+	}
+
+}
